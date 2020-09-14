35fba260256ceed30bc765204afe9671da21deab
bocastle <bocastle1213@gmail.com>
2020-09-14 19:54:01 +0900

    LifeCycle Random Color commit

--- src/App.js
+++ src/App.js
@@ -2,20 +2,34 @@ import React, { Component } from 'react';
 import ShoppingList from './Component/shoppingList'
 import './App.css';
 import Game from './Component/game';
+import LifeCycle from './Component/LifeCycle';
 
-
+function getRandomColor(){
+  return '#' + Math.floor(Math.random()*16777215).toString(16);
+}
 class App extends Component{
+  state = {
+    color : '#000000'
+  }
+handleClick = () => {
+  this.setState({
+    color : getRandomColor()
+  });
+}
 
   render(){
 
     return(
       <>
-      <ShoppingList />
-      
-      <Game />
-     
+        <div>
+        <ShoppingList />
+        <Game />
+        </div>
+        <div>
+        <button onClick={this.handleClick}>Random Color</button>
+        <LifeCycle color={this.state.color} />
+        </div>
       </>
-
     );
   }
 }
